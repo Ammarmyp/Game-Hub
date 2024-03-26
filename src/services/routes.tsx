03f242290ad5fd765e7820c0e6eb51976a71c449
Ammarmyp <ammarmyp@gmail.com>
2024-03-26 15:07:30 +0300

    Fetch Game Details

--- src/services/routes.tsx
+++ src/services/routes.tsx
@@ -1,20 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "../pages/Layout";
 import HomePage from "../pages/HomePage";
-import GameDetail from "../pages/GameDetail";
+import GameDetailPage from "../pages/GameDetailPage";
 import ErrorPage from "../pages/ErrorPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    errorElement: <ErrorPage/>,
+    errorElement: <ErrorPage />,
     children: [
       { index: true, element: <HomePage /> },
-      { path: "games/:id", element: <GameDetail /> },
+      { path: "games/:slug", element: <GameDetailPage /> },
     ],
   },
-
 ]);
 
 export default router;
